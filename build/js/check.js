60c65485b32c7d932e148706548ae2ceb3299e5c
SvyatoslavF <svyatoslav.fedorov@gmail.com>
2016-06-02 11:40:00 +0300

    Модуль 1 Задание 2
    Исправил код *после диалога в скайпе*.
    Синтаксис, опечатки, структура.

--- build/js/check.js
+++ build/js/check.js
@@ -1,32 +1,33 @@
-var getMessage = function(a, b){
-	if(tipeof(a)=="boolean"){
-		if(a==true){
-			textMessage = "Переданное GIF-изображение анимировано и содержит [b] кадров";
+var getMessage = function(a, b) {
+	if (typeof a === 'boolean') {
+		if (a) {
+			return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
 		}
-		else if(a==false){
-			textMessage = "Переданное GIF-изображение не анимировано";
-		}
+		return 'Переданное GIF-изображение не анимировано';
 	}
 
-	else if(tipeof(a)=="number"){
-		textMessage = "Переданное SVG-изображение содержит [a] объектов и [b * 4] атрибутов";
+	if (typeof a === 'number') {
+		return 'Переданное SVG-изображение содержит' + a + ' объектов и ' + b * 4 + ' атрибутов';
 	}
 
-	else if((tipeof(a)=="object")&&(tipeof(b)=="object")){
-		var square = 0;
-		for(var i=0; (i<= a.length) || (i<= b.length); i++){
-			square = square + a[i]*b[i];
+	if (Array.isArray(a)) {
+		if (Array.isArray(b)) {
+
+			var square = 0;
+			for (var i = 0; i < a.length; i++) {
+				square = square + a[i] * b[i];
+			}
+
+		return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
 		}
-		textMessage = "Общая площадь артефактов сжатия: [square] пикселей";
-	}
 
-	else if(tipeof(a)=="object"){
+	
 		var sum = 0;
-		for (var i=0; i<= a.length; i++){
+		for (var i = 0; i < a.length; i++) {
 			sum = sum + a[i];
 		}
-		textMessage = "Количество красных точек во всех строчках изображения: [sum]";
+		return 'Количество красных точек во всех строчках изображения: ' + sum;
 	}
-}
+};
 
-getMessage(2, 4);
+getMessage();
